763cb77808146267c3dd7e9ef9f1afb92e812480
Andrew William Watson <aww@awwsmm.com>
2025-08-25 09:12:34 +0100

    minimal web server per https://doc.rust-lang.org/book/ch21-01-single-threaded.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let _: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+
+    let contents = r#"<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <title>Hello!</title>
+          </head>
+          <body>
+            <h1>Hello!</h1>
+            <p>Hi from Rust</p>
+          </body>
+        </html>
+    "#;
+
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
