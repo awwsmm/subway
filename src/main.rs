accdff12a4c6e799be3e1a7fa5fd26a36c0ad8a4
Andrew William Watson <aww@awwsmm.com>
2025-08-29 14:22:02 +0100

    use Salvo for routing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
+use salvo::catcher::Catcher;
+use salvo::prelude::*;
 use std::fs;
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+#[handler]
+async fn hello(res: &mut Response) {
+    let filename = "resources/hello.html";
+    let contents = fs::read_to_string(filename).unwrap();
+    res.render(Text::Html(contents))
+}
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+#[handler]
+async fn not_found(&self, _req: &Request, _depot: &Depot, res: &mut Response, ctrl: &mut FlowCtrl) {
+    if StatusCode::NOT_FOUND == res.status_code.unwrap_or(StatusCode::NOT_FOUND) {
+        let filename = "resources/404.html";
+        let contents = fs::read_to_string(filename).unwrap();
+        res.render(Text::Html(contents));
 
-        handle_connection(stream);
+        // Skip remaining error handlers
+        ctrl.skip_rest();
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "resources/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "resources/404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+#[tokio::main]
+async fn main() {
+    let acceptor = TcpListener::new("0.0.0.0:8989").bind().await;
+    let router = Router::new().get(hello);
+    let catcher = Catcher::default().hoop(not_found);
+    Server::new(acceptor).serve(Service::new(router).catcher(catcher)).await;
 }
