e85576e00a8c63b3c9c507108bc9e2d95f1686f1
Andrew William Watson <aww@awwsmm.com>
2025-10-19 09:50:52 -0300

    saving progress before I break anything -- most stuff works, but app needs explicit refresh on close-reopen to reload user info from Keycloak

--- frontend/src/auth/AuthContextKeycloak.tsx
+++ frontend/src/auth/AuthContextKeycloak.tsx
@@ -10,30 +10,6 @@ const _keycloak = new Keycloak({
 
 export const AuthContextKeycloakProvider = ({ children }: { children: React.ReactNode }) => {
 
-    // alert("AuthContextKeycloakProvider constructed")
-
-    /*
-
-
-{state: "9b40d7af-08c2-45fe-8ffb-69e8bf127662", nonce: "4985be54-edca-44a3-9a7b-6cf1799781ae",…}
-expires
-:
-1760792698270
-nonce
-:
-"4985be54-edca-44a3-9a7b-6cf1799781ae"
-pkceCodeVerifier
-:
-"gqPkk4GkzIFhNOcDLBqhrNO57Nvi5gkoVUekzHDT2wJoqJ4ePswhyOc8qa8WUxxEMGO8Z0AZCjqe0MSimpOadGgnlGP8RHil"
-redirectUri
-:
-"http%3A%2F%2Flocalhost%3A5173%2Flogin"
-state
-:
-"9b40d7af-08c2-45fe-8ffb-69e8bf127662"
-
-     */
-
     const [user, setUser] = useState(() => {
         const maybeUser = sessionStorage.getItem('subway-user');
         return maybeUser || undefined;
@@ -48,34 +24,11 @@ state
         }
     }, [user]); // Dependency array: effect runs when 'user' changes
 
-
-    // TODO works as expected!
     _keycloak.onAuthSuccess = () => {
-        // alert(`keycloak auth success with user ${_keycloak.tokenParsed?.preferred_username}`)
         if (_keycloak.tokenParsed?.preferred_username && _keycloak.tokenParsed?.preferred_username !== user) {
             setUser(_keycloak.tokenParsed?.preferred_username);
         }
     }
-
-    // TODO works as expected!
-    _keycloak.onReady = () => {
-        // alert(`keycloak ready with user ${_keycloak.tokenParsed?.preferred_username}`)
-        if (_keycloak.tokenParsed?.preferred_username && _keycloak.tokenParsed?.preferred_username !== user) {
-            setUser(_keycloak.tokenParsed?.preferred_username);
-        }
-    }
-
-
-    //
-    // const [initialized, setInitialized] = useState(() => {
-    //     const initialized = sessionStorage.getItem('subway-initialized');
-    //     return initialized ? JSON.parse(initialized) : false;
-    // });
-    //
-    // // Use useEffect to save state to sessionStorage whenever 'initialized' changes
-    // useEffect(() => {
-    //     sessionStorage.setItem('subway-initialized', JSON.stringify(initialized));
-    // }, [initialized]); // Dependency array: effect runs when 'initialized' changes
 
     if (!_keycloak.didInitialize) {
         _keycloak
@@ -83,25 +36,10 @@ state
                 onLoad: "check-sso",
                 silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
             })
-            .then(() => {
-                // TODO works as expected!
-                // alert(`Keycloak initialized with user ${_keycloak.tokenParsed?.preferred_username}`);
-                if (_keycloak.tokenParsed?.preferred_username && _keycloak.tokenParsed?.preferred_username !== user) {
-                    setUser(_keycloak.tokenParsed?.preferred_username);
-                }
-                // setInitialized(true);
-                // setUser(_keycloak.tokenParsed?.preferred_username);
-            })
             .catch((err) => {
                 console.error("Keycloak init error:", err);
             })
     }
-
-    // alert(`username == ${_keycloak.tokenParsed?.preferred_username}, user = ${user}`)
-
-    // if (_keycloak.tokenParsed?.preferred_username !== user || (!!_keycloak.tokenParsed?.preferred_username && !!user) ) {
-    //     setUser(_keycloak.tokenParsed?.preferred_username || undefined);
-    // }
 
     // init() is called during login()
     const init = () => {
@@ -127,23 +65,17 @@ state
     }
 
     const logout = (_redirectUri: string) => {
-        setUser(undefined)
-        // setInitialized(false)
-        _keycloak.logout()
-            // .then(() => setUser(undefined))
-            // .then(() => setInitialized(false))
-            // .then(() => window.location.href = redirectUri)
-        // setUser(undefined);
-        // setInitialized(false);
-        // window.location.href = redirectUri;
+        setUser(undefined);
+        void _keycloak.logout();
     }
 
     const username = () => {
-        // return _keycloak.tokenParsed?.preferred_username || undefined;
         return user
     }
 
     const hasRole = (roles: string[]) => {
+
+        alert(`roles: ${_keycloak.tokenParsed}`)
 
         const userRoles = _keycloak.tokenParsed?.realm_access?.roles || [];
         return (userRoles && userRoles.some(role => roles.includes(role))) || false;
